5e745a085228cb562509643c52335a268baee77a
Joe Wemyss <joewemyss3@gmail.com>
2015-06-27 14:24:42 +0100

    Add files

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,15 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'MainCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
-}]);
+.controller('MainCtrl', ['$scope', '$rootScope',function($scope, $rootScope) {
+	$scope.title = 'Home';
+	$scope.name = 'World';
+	$rootScope.department = 'angular';
+}])
+.controller('SecondCtrl', ['$scope', function($scope){
+	$scope.names = ['Joe', 'Michelle', 'Joseph'];
+}])
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,10 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', ['$scope', function($scope) {
+	$scope.title = 'About';
+	$scope.username = 'World';
+	$scope.sayHello = function(){
+		$scope.greeting = 'Hello ' + $scope.username + '!';
+	}
 }]);
