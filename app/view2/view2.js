e83dc89d54b2515fd2d1832a2a90e07615db08b0
Joe Wemyss <joewemyss3@gmail.com>
2015-06-27 23:38:36 +0100

    update

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,18 +1,23 @@
 'use strict';
-
+//Define module
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
-
-.controller('View2Ctrl', ['$scope', function($scope) {
-	$scope.title = 'About';
-	$scope.username = 'World';
-	$scope.sayHello = function(){
-		$scope.greeting = 'Hello ' + $scope.username + '!';
-	}
-}]);
+    //Define route
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
+        });
+    }])
+    //Define controller
+    .controller('View2Ctrl', ['$scope', function ($scope) {
+        //Variables for this scope
+        $scope.title = 'About';
+        $scope.username = 'World';
+        //This function will be called when the button is clicked
+        $scope.sayHello = function () {
+           /* This is the variable that will be displayed when the
+            * button is clicked */
+            $scope.greeting = 'Hello ' + $scope.username + '!';
+        }
+    }]);
